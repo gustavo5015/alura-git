88e72144ddc0840b4d79a069326b2d667722ca31
Gustavo <gustavomendes5015@gmail.com>
2022-12-30 14:13:17 -0300

    Criando arquivos para pesquisar CEP

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+const preencherFormulario = (endereco) => {
+    const logradouro = document.getElementById('endereco')
+    const bairro = document.getElementById('bairro')
+    const cidade = document.getElementById('cidade')
+    const uf = document.getElementById('estado')
+
+    logradouro.value = endereco.logradouro
+    bairro.value = endereco.bairro
+    cidade.value = endereco.localidade
+    uf.value = endereco.uf
+
+}
+
+const pesquisarCEP = async () => {
+    const cep = document.getElementById('cep').value
+    if(typeof cep === 'number' && isFinite(value)) {
+        const url = `https://viacep.com.br/ws/${cep}/json/`
+        const dados = await fetch(url)
+        const endereco = await dados.json()
+        preencherFormulario(endereco)
+    } else return alert('inform')
+    
+}
+
+document.getElementById('cep').addEventListener('focusout', pesquisarCEP)
